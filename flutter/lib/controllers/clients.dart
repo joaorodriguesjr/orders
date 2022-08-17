18636ee2e49cca265ae574f6abf208f2dc53c3c2
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-08-17 12:29:58 -0300

    add clients control abstractions

---
+++ flutter/lib/controllers/clients.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:delivery/clients.dart';
+
+class ClientsController extends ChangeNotifier {
+  List<Client> clients = [];
+
+  ClientsDataSource dataSource;
+
+  ClientsController(this.dataSource) {
+    dataSource.all().listen(_updateClients);
+  }
+
+  _updateClients(clients) {
+    this.clients = clients;
+    notifyListeners();
+  }
+
+  registerClient(Client client) async {
+    await dataSource.createClient(client);
+  }
+}
---
+++ flutter/lib/data/firestore_clients.dart
@@ -0,0 +1,46 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:delivery/clients.dart';
+
+class FirestoreClients implements ClientsDataSource {
+  final collection = FirebaseFirestore.instance.collection('clients');
+
+  @override
+  Stream<List<Client>> all() {
+    return collection.snapshots().map(clientsFromSnapshot);
+  }
+
+  @override
+  Future<void> createClient(Client client) async {
+    var doc = await collection.add({
+      'name': client.name,
+      'phone': client.phone,
+      'address': {
+        'description': client.address.description,
+        'complement': client.address.complement,
+      },
+    });
+
+    client.id = doc.id;
+  }
+
+  @override
+  Future<void> updateClient(Client client) async {}
+
+  @override
+  Future<void> deleteClient(Client client) async {}
+}
+
+List<Client> clientsFromSnapshot(QuerySnapshot snapshot) {
+  return snapshot.docs.map(clientFromDocument).toList();
+}
+
+Client clientFromDocument(DocumentSnapshot doc) {
+  var data = doc.data() as Map<String, dynamic>;
+
+  return Client()
+    ..id = doc.id
+    ..name = data['name']
+    ..phone = data['phone']
+    ..address.description = data['address']['description']
+    ..address.complement = data['address']['complement'];
+}
