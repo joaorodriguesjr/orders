1341d3d9168f3d580de1884c5dfeb6392da9744d
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-08-17 16:02:07 -0300

    rename directory

--- flutter/lib/controllers.dart
+++ flutter/lib/controllers.dart
@@ -1,8 +1,8 @@
 export 'controllers/products.dart';
-export 'data/firestore_products.dart';
+export 'models/firestore_products.dart';
 
 export 'controllers/clients.dart';
-export 'data/firestore_clients.dart';
+export 'models/firestore_clients.dart';
 
 export 'controllers/orders.dart';
-export 'data/firestore_orders.dart';
+export 'models/firestore_orders.dart';
