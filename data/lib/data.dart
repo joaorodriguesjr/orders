f6620a0991b26f7ffac9faaa4cb0e6e585b3e4ad
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-09-01 13:51:36 -0300

    add Firestore implementation

--- data/lib/data.dart
+++ data/lib/data.dart
@@ -1,3 +1,5 @@
+export 'inventory/firestore_products.dart';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
---
+++ data/lib/inventory/firestore_products.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:inventory/inventory.dart';
+
+class FirestoreProducts implements ProductsQueries {
+  final CollectionReference collection;
+
+  FirestoreProducts(this.collection);
+
+  @override
+  Stream<List<Product>> allProducts() {
+    return collection.orderBy('name').snapshots().map(productsFromSnapshot);
+  }
+}
+
+List<Product> productsFromSnapshot(QuerySnapshot snapshot) {
+  return snapshot.docs.map(productFromDocument).toList();
+}
+
+Product productFromDocument(DocumentSnapshot doc) {
+  var data = doc.data() as Map<String, dynamic>;
+  return Product.restore(doc.id, data);
+}
