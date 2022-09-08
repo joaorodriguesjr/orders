210db5f8c3be25d9daea594125ba805038af1abf
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-09-08 14:43:03 -0300

    add persistence implementation

--- data/lib/inventory/firestore_products.dart
+++ data/lib/inventory/firestore_products.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:inventory/inventory.dart';
 
-class FirestoreProducts implements ProductsQuery {
+class FirestoreProducts implements ProductsQuery, ProductsPersistence {
   final CollectionReference collection;
 
   FirestoreProducts(this.collection);
@@ -9,6 +9,13 @@ class FirestoreProducts implements ProductsQuery {
   @override
   Stream<List<Product>> allProducts() {
     return collection.orderBy('name').snapshots().map(productsFromSnapshot);
+  }
+
+  @override
+  Future<void> persist(Product product) async {
+    await collection
+        .doc(product.code)
+        .set({'name': product.name, 'description': product.description});
   }
 }
 
