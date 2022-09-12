ffda5e035a11ad740aa3cceb6325a73c3620dd07
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-09-12 08:36:37 -0300

    export product selection

--- interface/lib/controllers.dart
+++ interface/lib/controllers.dart
@@ -1,4 +1,5 @@
 export 'inventory/controllers/dashboard.dart';
 export 'inventory/controllers/list_products.dart';
 export 'inventory/controllers/create_product.dart';
+export 'inventory/controllers/select_product.dart';
 export 'inventory/controllers/create_entry.dart';
--- interface/lib/views.dart
+++ interface/lib/views.dart
@@ -1,4 +1,5 @@
 export 'inventory/views/dashboard.dart';
 export 'inventory/views/list_products.dart';
 export 'inventory/views/create_product.dart';
+export 'inventory/views/select_product.dart';
 export 'inventory/views/create_entry.dart';
