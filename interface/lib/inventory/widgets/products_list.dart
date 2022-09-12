b1d8bfd8db22cbabbfba5e7cad55c7bcc105ff07
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-09-12 08:59:02 -0300

    adjust list physics

--- interface/lib/inventory/widgets/products_list.dart
+++ interface/lib/inventory/widgets/products_list.dart
@@ -14,6 +14,7 @@ class ProductsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView.separated(
       shrinkWrap: true,
+      physics: const ClampingScrollPhysics(),
       itemBuilder: (context, index) {
         var product = products[index];
         return ListTile(
