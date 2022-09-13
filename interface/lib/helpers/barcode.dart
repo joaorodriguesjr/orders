01679f36adf8f797306deae82550bcf8430e9306
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-09-13 08:00:49 -0300

    activate flashlight option

--- interface/lib/helpers/barcode.dart
+++ interface/lib/helpers/barcode.dart
@@ -2,5 +2,5 @@ import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 Future<String> barcodeScan() async {
   return FlutterBarcodeScanner.scanBarcode(
-      '#ff6666', 'Cancelar', false, ScanMode.DEFAULT);
+      '#ff6666', 'Cancelar', true, ScanMode.DEFAULT);
 }
