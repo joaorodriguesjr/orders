f0aacfac8a527dcc5a6116d0152a01a2076b0f03
joaorodriguesjr <mail@joaorodriguesjr.com>
2022-09-13 08:06:39 -0300

    add custom icons

---
+++ interface/lib/icons.dart
@@ -0,0 +1,14 @@
+export 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:flutter/widgets.dart';
+
+class CustomIcons {
+  CustomIcons._();
+
+  static const String _fontFamily = 'inventory';
+
+  static const IconData inventory_in =
+      IconData(0xe900, fontFamily: _fontFamily);
+  static const IconData inventory_ou =
+      IconData(0xe901, fontFamily: _fontFamily);
+  static const IconData add_product = IconData(0xe902, fontFamily: _fontFamily);
+}
